da8b5df6b6389c98b6cc4c18ef8b1da94257c965
Prince-213 <princolosh@gmail.com>
2024-06-28 21:52:01 +0100

    check

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/checkout")) {
+  if (
+    request.nextUrl.pathname.startsWith("/checkout") ||
+    request.nextUrl.pathname.startsWith("/checkout/success")
+  ) {
     if (!request.cookies.get("auth")) {
-      return NextResponse.redirect(new URL("/login"));
+      return NextResponse.redirect(new URL("/login", request.url));
     }
   }
 }
